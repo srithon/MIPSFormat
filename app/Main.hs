15f59618c15cd05971f409a93e745587f4b8db8e
Sridaran Thoniyil <sri7thon@gmail.com>
2022-05-16 01:54:51 -0400

    Add CLI code to Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,39 @@
 module Main where
 
+import Data.Semigroup ((<>))
 import qualified Data.Text as T
 import qualified Data.Text.IO as TO
 import Lib
+import Options.Applicative
+
+data AppOptions = AppOptions
+  {debug :: Bool}
+
+appOptions :: Parser AppOptions
+appOptions =
+  AppOptions
+    <$> switch
+      ( long "debug"
+          <> short 'd'
+          <> help "If specified, prints out debug information rather than the formatted code."
+      )
+
+run :: AppOptions -> IO ()
+run opts = do
+  contents <- getContents
+  let text = T.pack contents
+  if debug opts
+    then debugCategorize text
+    else TO.putStrLn $ format text
 
 main :: IO ()
 main = do
-  contents <- getContents
-  let text = T.pack contents
-  TO.putStrLn $ format text
+  run =<< execParser opts
+  where
+    opts =
+      info
+        -- add help message to parser
+        (helper <*> appOptions)
+        ( fullDesc
+            <> progDesc "A basic, opinionated formatter for MIPS assembly."
+        )
